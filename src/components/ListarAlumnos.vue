e512caccc66c929fbcd6046b0d39cd3796e3045a
alejandropj <alejandroparrajimenez@gmail.com>
2023-11-18 21:08:28 +0100

    18/11 v3

--- src/components/ListarAlumnos.vue
+++ src/components/ListarAlumnos.vue
@@ -14,17 +14,73 @@
       </thead>
       <tbody>
         <tr v-for="alumno in alumnos" :key="alumno">
-          <td>{{alumno.idAlumno}}</td>
-          <td>{{alumno.nombre}}</td>
-          <td>{{alumno.apellidos}}</td>
-          <td><img :src="alumno.imagen" style="width:150px;height:150px"/></td>
-          <td>{{alumno.activo}}</td>
-          <td>{{alumno.idCurso}}</td>
+          <td>{{ alumno.idAlumno }}</td>
+          <td>{{ alumno.nombre }}</td>
+          <td>{{ alumno.apellidos }}</td>
           <td>
-            <router-link class="btn btn-info" :to="'/detalle/'+alumno.idAlumno">Detalle</router-link>
-            <router-link class="btn btn-warning" :to="'/modificar/'+alumno.idAlumno">Actualizar</router-link>
-            <router-link class="btn btn-danger" :to="'/eliminar/'+alumno.idAlumno">Eliminar</router-link>
-            </td>
+            <img :src="alumno.imagen" style="width: 150px; height: 150px" />
+          </td>
+          <td>{{ alumno.activo }}</td>
+          <td>{{ alumno.idCurso }}</td>
+          <td>
+            <router-link
+              class="btn btn-info"
+              :to="'/detalle/' + alumno.idAlumno"
+              >Detalle</router-link
+            >
+            <router-link
+              class="btn btn-warning"
+              :to="'/modificar/' + alumno.idAlumno"
+              >Actualizar</router-link
+            >
+            <!-- Button trigger modal -->
+            <button
+              type="button"
+              class="btn btn-danger"
+              data-bs-toggle="modal"
+              data-bs-target="#exampleModal"
+            >
+              Eliminar
+            </button>
+
+            <!-- Modal -->
+            <div
+              class="modal fade"
+              id="exampleModal"
+              tabindex="-1"
+              aria-labelledby="exampleModalLabel"
+              aria-hidden="true"
+            >
+              <div class="modal-dialog">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">
+                      Eliminar {{alumno.nombre}}
+                    </h1>
+                    <button
+                      type="button"
+                      class="btn-close"
+                      data-bs-dismiss="modal"
+                      aria-label="Close"
+                    ></button>
+                  </div>
+
+                  <div class="modal-footer">
+                    <button
+                      type="button"
+                      class="btn btn-secondary"
+                      data-bs-dismiss="modal"
+                    >
+                      Volver
+                    </button>
+                    <button type="button" class="btn btn-primary" @click="eliminarAlumno(alumno.idAlumno)">
+                      Eliminar
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </td>
         </tr>
       </tbody>
     </table>
@@ -32,24 +88,28 @@
 </template>
 
 <script>
-import ServiceAlumnos from '@/services/ServiceAlumnos'
+import ServiceAlumnos from "@/services/ServiceAlumnos";
 const service = new ServiceAlumnos();
 
 export default {
-    name:"ListarAlumnos",
-    data(){
-      return{
-        alumnos:null
-      }
-    },
-    mounted(){
-      service.getAlumnos().then(res=>{
-        this.alumnos=res.data
-      })
+  name: "ListarAlumnos",
+  data() {
+    return {
+      alumnos: null,
+    };
+  },
+  mounted() {
+    service.getAlumnos().then((res) => {
+      this.alumnos = res.data;
+    });
+  },
+  methods:{
+    eliminarAlumno(idAlumno){
+      alert("Eliminado "+idAlumno);
     }
-}
+  }
+};
 </script>
 
 <style>
-
 </style>
